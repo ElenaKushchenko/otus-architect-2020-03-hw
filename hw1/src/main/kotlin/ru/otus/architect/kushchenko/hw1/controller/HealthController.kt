5892a9122e5dc815a53a83e75db246624c1032bc
Elena Kushchenko <elena_kushchenko@sberbank-cib.ru>
2020-04-14 02:58:16 +0300

    add dockerfile

--- hw1/src/main/kotlin/ru/otus/architect/kushchenko/hw1/controller/HealthController.kt
+++ hw1/src/main/kotlin/ru/otus/architect/kushchenko/hw1/controller/HealthController.kt
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloController {
+class HealthController {
 
-    @GetMapping("hello")
-    fun getHello() = "Hello kitty!"
+    @GetMapping("health")
+    fun getOk() = mapOf("status" to "OK")
 }
